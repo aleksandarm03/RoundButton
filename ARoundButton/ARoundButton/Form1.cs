20a58aa3bcd5acf4788275896c2859037dd6736d
aleksandarm03 <accam003@gmail.com>
2021-02-03 11:54:16 +0100

    update 03.02.2021.

---
+++ ARoundButton/ARoundButton/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ARoundButton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RoundButton rb = new RoundButton();
+            rb.Click += new EventHandler(this.roudButton_Click);
+            this.Controls.Add(rb);
+        }
+        private void roudButton_Click(object sender, System.EventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
+    }
+}
---
+++ ARoundButton/ARoundButton/RoundButton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+namespace ARoundButton
+{
+    class RoundButton:Button
+    {
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            this.Size = new Size(50, 50);
+            System.Drawing.Drawing2D.GraphicsPath aCircle = new System.Drawing.Drawing2D.GraphicsPath();
+            aCircle.AddEllipse(new System.Drawing.RectangleF(0, 0, 50, 50));
+            this.Region = new Region(aCircle);
+        }
+    }
+}
